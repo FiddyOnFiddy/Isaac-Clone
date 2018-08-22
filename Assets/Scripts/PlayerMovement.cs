dced27c291bd00c3b0119e79b8fefefa75c7332c
MattBee2k2 <matthewbiggins@hotmail.co.uk>
2018-08-22 12:51:29 +0100

    health partial fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,10 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed;
     public Vector2 moveVelocity;
 
-    public Canvas canvas;
     public Sprite halfHeartContainer;
     public Sprite emptyHeartContainer;
-
     public Sprite heartSprite;
 
     public Image[] heartContainers;
@@ -26,9 +24,6 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        heartContainers = new Image[11];
-
-        heartContainers = canvas.GetComponentsInChildren<Image>();
         playerStats = GetComponent<PlayerStatsScript>();
 
         
@@ -38,18 +33,37 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (playerStats.heartContainers <= 12)
-        {
-            for (int i = 0; i < playerStats.heartContainers; i++)
-            {
-                heartContainers[i].color = new Vector4(heartContainers[i].color.r, heartContainers[i].color.b, heartContainers[i].color.g, 255.0f);
-            }
-        }
-       
+
+        ShowHeartContainers();
 
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * movementSpeed;
 
+    }
+
+    public void ShowHeartContainers()
+    {
+        for (int i = 0; i < heartContainers.Length; i++)
+        {
+            if (i < playerStats.health)
+            {
+                heartContainers[i].sprite = heartSprite;
+            }
+            else
+            {
+                heartContainers[i].sprite = emptyHeartContainer;
+            }
+
+
+            if (i < playerStats.heartContainers)
+            {
+                heartContainers[i].enabled = true;
+            }
+            else
+            {
+                heartContainers[i].enabled = false;
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -65,43 +79,26 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Enemy" && playerStats.health % 2 == 0)
+        if (collision.tag == "Enemy")
         {
-            Debug.Log("collided");
             playerStats.health -= 1;
-
-            heartContainers[playerStats.health / 2 ].sprite = halfHeartContainer;
-        }
-        else if (collision.tag == "Enemy" && playerStats.health % 2 == 1)
-        {
             Debug.Log("collided");
-            playerStats.health -= 1;
-
-            heartContainers[playerStats.health / 2].sprite = emptyHeartContainer;
         }
 
         if (collision.tag == "Heart")
         {
-            playerStats.health += 2;
 
         }
 
         if (collision.tag == "HpUp")
         {
-            if (playerStats.heartContainers <= 11)
-            {
-                playerStats.heartContainers += 1;
-                playerStats.health += 2;
-            }
+            playerStats.heartContainers += 1;
+            playerStats.health += 1;
         }
 
         if (collision.tag == "HpDown")
         {
-            if (playerStats.heartContainers >= 1)
-            {
-                playerStats.heartContainers -= 1;
-            }
-            heartContainers[playerStats.heartContainers].color = new Vector4(heartContainers[playerStats.heartContainers].color.r, heartContainers[playerStats.heartContainers].color.b, heartContainers[playerStats.heartContainers].color.g, 0.0f);
+            playerStats.heartContainers -= 1;
         }
     }
 }
